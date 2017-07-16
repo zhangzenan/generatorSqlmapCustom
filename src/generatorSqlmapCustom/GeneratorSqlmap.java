2d49721793dfc0f71afd51dcefe1a821dec6e5ff
zhangzenan <zzn1991129@126.com>
2017-07-16 17:34:48 +0800

    逆向工程

---
+++ src/generatorSqlmapCustom/GeneratorSqlmap.java
@@ -0,0 +1,32 @@
+package generatorSqlmapCustom;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.security.auth.login.Configuration;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+public class GeneratorSqlmap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void generator() {
+		/*List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		File configFile = new File("generatorConfig.xml");
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(configFile);
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+		myBatisGenerator.generate(null);*/
+
+	}
+
+}
