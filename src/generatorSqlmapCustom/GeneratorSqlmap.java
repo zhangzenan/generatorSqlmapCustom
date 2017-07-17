f24e39903eed943640f87c2b5b98fd5bbc5d7302
zhangzenan <zhangzenan@yiguo.com>
2017-07-17 12:52:32 +0800

    测试逆向工程

--- src/generatorSqlmapCustom/GeneratorSqlmap.java
+++ src/generatorSqlmapCustom/GeneratorSqlmap.java
@@ -4,29 +4,33 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.security.auth.login.Configuration;
-
 import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
 public class GeneratorSqlmap {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-
+		try {
+			GeneratorSqlmap generatorSqlmap = new GeneratorSqlmap();
+			generatorSqlmap.generator();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	public void generator() {
-		/*List<String> warnings = new ArrayList<String>();
+	public void generator() throws Exception {
+
+		List<String> warnings = new ArrayList<String>();
 		boolean overwrite = true;
+		// 指定 逆向工程配置文件
 		File configFile = new File("generatorConfig.xml");
 		ConfigurationParser cp = new ConfigurationParser(warnings);
 		Configuration config = cp.parseConfiguration(configFile);
 		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
 		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
-		myBatisGenerator.generate(null);*/
+		myBatisGenerator.generate(null);
 
 	}
 
